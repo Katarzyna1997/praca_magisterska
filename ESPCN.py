4672f35b47a59b09392ac9c37f5a4c96c57dca7d
Katarzyna1997 <48634097+Katarzyna1997@users.noreply.github.com>
2021-04-20 16:45:27 +0200

    Update ESPCN.py

--- ESPCN.py
+++ ESPCN.py
@@ -45,24 +45,6 @@ def denormalize(x, rgb_mean=DIV2K_RGB_MEAN):
     return x * 127.5 + rgb_mean
 
 
-def normalize_01(x):
-    """Normalizes RGB images to [0, 1]."""
-    return x / 255.0
-
-
-def normalize_m11(x):
-    """Normalizes RGB images to [-1, 1]."""
-    return x / 127.5 - 1
-
-
-def denormalize_m11(x):
-    """Inverse of normalize_m11."""
-    return (x + 1) * 127.5
-
-
-def psnr(x1, x2):
-    return tf.image.psnr(x1, x2, max_val=255)
-
 def pixel_shuffle(scale):
     return lambda x: tf.nn.depth_to_space(x, scale)
 
